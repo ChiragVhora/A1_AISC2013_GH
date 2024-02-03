6acd45a6d9360895a909ffad3c26748168272eca
Chirag Vhora <Chiragvhora92@gmail.com>
2024-02-04 03:58:20 +0530

    Add new feature to AI solution Python script

--- ai_script.py
+++ ai_script.py
@@ -2,5 +2,11 @@ def my_ai_solution():
     print("training dataset...")
     print("model build done !")
 
+def new_feature_solution():
+    print("This is a new feature!")
+    print("cleaning dataset...")
+    print("training dataset...")
+    print("model build done !")
+
 if __name__ == "__main__":
     my_ai_solution()
